fd356afabce6cb37f479109e24f9cade26180e14
Takatoshi-Miura <takatoshi052031@gmail.com>
2023-09-17 13:06:17 +0900

    初期コミット

---
+++ SwiftAIDemo/SwiftAIDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAIDemo
+//
+//  Created by Takatoshi Miura on 2023/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
